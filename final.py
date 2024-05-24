723be4336623af90e2e02ac1af5e8d7409af621b
martin <martintefra@gmail.com>
2024-05-24 13:38:42 +0200

    works

--- final.py
+++ final.py
@@ -6,6 +6,7 @@ from transformers import ViltProcessor, ViltModel, get_scheduler, AdamW
 from torch.nn.utils.rnn import pad_sequence
 
 from models import MultimodalDataset
+from sklearn.utils import resample
 
 
 # Load the model
@@ -15,6 +16,10 @@ processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-nlvr2")
 # else "mps" if torch.backends.mps.is_available() 
 device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 model.to(device)
+
+# Classifier added to the model
+num_classes = 2
+classifier = nn.Linear(model.config.hidden_size, num_classes).to(device)
 
 def collate_fn(batch):
     # Extract features and labels from batch
@@ -40,6 +45,9 @@ def collate_fn(batch):
 
 dataset = MultimodalDataset(csv_file='data/MMHS150K_GT.csv', processor=processor)
 
+# take subset of dataset
+dataset = resample(dataset, n_samples=10000, random_state=42)
+
 train_size = int(0.8 * len(dataset))
 val_size = (len(dataset) - train_size) // 2
 test_size = len(dataset) - train_size - val_size
@@ -50,13 +58,6 @@ batch_size = 16
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,  collate_fn=collate_fn)
 val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False,  collate_fn=collate_fn)
 test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False,  collate_fn=collate_fn)
-
-
-####
-batch = next(iter(train_dataloader))
-for k,v in batch.items():
-    print(k, v.shape)
-#####
 
 
 # Optimizer
@@ -75,11 +76,11 @@ scheduler = get_scheduler(
 
 # Training loop
 best_val_loss = float('inf')
-nn.CrossEntropyLoss()
 
 for epoch in range(num_epochs):
     model.train()
     total_loss = 0
+    total_correct = 0
     for batch in tqdm.tqdm(train_dataloader, desc='Training'):
         batch = {k:v.to(device) for k,v in batch.items()}
 
@@ -97,17 +98,25 @@ for epoch in range(num_epochs):
         
         pooler_output = outputs.pooler_output
         
-        num_classes = 2
-        classifier = nn.Linear(pooler_output.shape[-1], num_classes).to(device)
-        logits = classifier(pooler_output).to(device)
+        logits = classifier(pooler_output)
         
         loss = criterion(logits, batch["labels"].long())
+        print(loss)
         loss.backward()
         optimizer.step()
         scheduler.step()
         
         total_loss += loss.item()
         
+        # Calculate accuracy
+        preds = torch.argmax(logits, dim=1)
+        total_correct += torch.sum(preds == batch["labels"]).item()
+    
+    train_loss = total_loss / len(train_dataloader)
+    train_acc = total_correct / len(train_dataset)
+    print(f'Epoch {epoch + 1}/{num_epochs}')
+    print(f'Training Loss: {train_loss:.4f}')
+    print(f'Training Accuracy: {train_acc:.4f}')
         
         
     model.eval()
@@ -115,27 +124,30 @@ for epoch in range(num_epochs):
     total_correct = 0
     with torch.no_grad():
         for batch in tqdm.tqdm(val_dataloader, desc='Evaluating'):
-            image, text, labels = batch["image"], batch["text"], batch["label"]
-
-            encoding = processor(image, text, return_tensors='pt', padding=True, truncation=True, max_length=40)
-
-            outputs = model(**encoding)
-            logits = outputs.logits
-            loss = criterion(logits, labels)
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
-
+            batch = {k:v.to(device) for k,v in batch.items()}
+            optimizer.zero_grad()
+            outputs = model(
+                input_ids=batch["input_ids"],
+                pixel_values=batch["pixel_values"],
+                attention_mask=batch["attention_mask"],
+                token_type_ids=batch["token_type_ids"],
+                pixel_mask=batch["pixel_mask"]
+            ) 
+            
+            pooler_output = outputs.pooler_output
+            
+            logits = classifier(pooler_output)
+            
+            loss = criterion(logits, batch["labels"].long())
+        
             total_loss += loss.item()
-
             preds = torch.argmax(logits, dim=1)
-            total_correct += torch.sum(preds == labels).item()
+            total_correct += torch.sum(preds == batch["labels"]).item()
+            
 
     val_loss = total_loss / len(val_dataloader)
     val_acc = total_correct / len(val_dataset)
     print(f'Epoch {epoch + 1}/{num_epochs}')
-    print(f'Training Loss: {total_loss / len(train_dataloader):.4f}')
-    print(f'Training Accuracy: {total_correct / len(train_dataset):.4f}')
     print(f'Validation Loss: {val_loss:.4f}')
     print(f'Validation Accuracy: {val_acc:.4f}')
     print()
@@ -149,13 +161,21 @@ model.eval()
 total_correct = 0
 with torch.no_grad():
     for batch in tqdm.tqdm(test_dataloader, desc='Testing'):
-        images, labels = batch
-        images = images.to(device)
-        labels = labels.to(device)
-
-        logits = model(images)
+        batch = {k:v.to(device) for k,v in batch.items()}
+        outputs = model(
+            input_ids=batch["input_ids"],
+            pixel_values=batch["pixel_values"],
+            attention_mask=batch["attention_mask"],
+            token_type_ids=batch["token_type_ids"],
+            pixel_mask=batch["pixel_mask"]
+        ) 
+        
+        pooler_output = outputs.pooler_output
+        
+        logits = classifier(pooler_output)
+        
         preds = torch.argmax(logits, dim=1)
-        total_correct += torch.sum(preds == labels).item()
+        total_correct += torch.sum(preds == batch["labels"]).item()
         
 test_acc = total_correct / len(test_dataset)
 print(f'Test Accuracy: {test_acc:.4f}')
